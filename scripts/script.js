0a82bdb7be4e476d14d0db7ffb25b6b20e2012d3
daanthai <74142357+daanthai@users.noreply.github.com>
2020-12-17 15:38:46 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var deButton = document.querySelector(".hamburgerMenu");
+
+function menuHeenEnWeer(){
+    let hetMenu = document.querySelector("form");
+    hetMenu.classList.toggle("toonMenu");
+}
+
+deButton.addEventListener("click", menuHeenEnWeer);
